1e6421ac4a0fd7b1643ce3d17f5f15d571e949a0
yoshida <c0a2316612@edu.teu.ac.jp>
2024-04-23 14:26:55 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,10 +1,11 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-DELTA = {
+DELTA = {#こうかトン移動用辞書
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5,0),
@@ -22,19 +23,29 @@ def main():
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
     tmr = 0
+    bomb_img = pg.Surface((20,20))
+    pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
+    bomb_img.set_colorkey((0, 0, 0))
+    bb_rect = bomb_img.get_rect()
+    bb_rect.center = random.randint(0,WIDTH), random.randint(0,HEIGHT)
+    vx = +5
+    vy = +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        screen.blit(bomb_img, bb_rect)
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for k, v in DELTA.items():
+        for k, v in DELTA.items():#こうかトン移動用
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        bb_rect.move_ip(vx, vy)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
